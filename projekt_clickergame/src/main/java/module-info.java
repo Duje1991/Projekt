b1310089ec61094c22463a21637ebf3aed9e8c2d
Duje1991 <bar230189@spengergasse.at>
2025-06-08 13:57:18 +0200

    Delete projekt_clickergame directory

--- projekt_clickergame/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module projekt_clickergame {
-    requires javafx.controls;
-
-    exports at.spengergasse.projekt_clickergame.Model;
-    exports at.spengergasse.projekt_clickergame.ViewControl;
-}
